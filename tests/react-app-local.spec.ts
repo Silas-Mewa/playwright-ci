ca05b1f3a1ef2fcea4ed22dd99830d1344b3fe97
Silas-Mewa <silas.boecher@mewa.de>
2024-11-25 15:38:00 +0100

    initial commit

---
+++ tests/react-app-local.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+import { beforeEach, describe } from 'node:test';
+
+describe("local tests", (page) => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+  })
+  
+  test('has title', async ({ page }) => {
+    await expect(page.getByRole('heading')).toContainText('Vite + React')
+  });
+  
+  test('has button', async ({ page }) => {
+    await expect(page.getByRole('button')).toContainText('count is')
+  });
+
+  test('can click', async ({ page }) => {
+    await expect(page.getByRole('button')).toContainText('count is 0')
+    await page.getByRole('button', {name: 'count is '}).click()
+    await expect(page.getByRole('button')).toContainText('count is 1')
+    await page.getByRole('button', {name: 'count is '}).click()
+    await expect(page.getByRole('button')).toContainText('count is 2')
+  });
+  
+
+
+  
+  test('can access swagger', async ({ page }) => {
+    await page.goto('http://localhost:8080/swagger-ui/index.html');
+    await expect(page.getByRole('heading', { name: 'OpenAPI definition v0 OAS' })).toContainText('OpenAPI definition')
+  });
+  
+  test('can access external links', async ({ page }) => {
+    await page.goto('http://google.com');
+  });
+})
+
+
+
